6e314171fb035c92cbaac46a3df408212a09a265
slasher <mcq.sejust@gmail.com>
2023-12-25 19:22:53 +0800

    feat(flashnode): update client cache when status changed
    Signed-off-by: slasher <mcq.sejust@gmail.com>

--- master/cluster.go
+++ master/cluster.go
@@ -100,8 +100,7 @@ type Cluster struct {
 	DecommissionDiskFactor       float64
 	S3ApiQosQuota                *sync.Map // (api,uid,limtType) -> limitQuota
 
-	flashNodeTopo       *flashNodeTopology
-	flashGroupRespCache atomic.Value // []byte
+	flashNodeTopo *flashNodeTopology
 }
 
 type followerReadManager struct {
@@ -341,7 +340,6 @@ func newCluster(name string, leaderInfo *LeaderInfo, fsm *MetadataFsm, partition
 	c.snapshotMgr.cluster = c
 	c.S3ApiQosQuota = new(sync.Map)
 	c.flashNodeTopo = newFlashNodeTopology()
-	c.flashGroupRespCache.Store([]byte(nil))
 	return
 }
 
@@ -3443,7 +3441,6 @@ func (c *Cluster) allFlashNodes() (flashNodes []proto.NodeView) {
 	})
 	return
 }
-
 
 // get metaNode with specified condition
 func (c *Cluster) getSpecifiedMetaNodes(zones map[string]struct{}, nodeSetIds map[uint64]struct{}) (metaNodes []*MetaNode) {
